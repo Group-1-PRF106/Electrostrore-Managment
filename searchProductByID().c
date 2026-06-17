226103eabdc57d7a60065ca5d4479203a7398019
htrong02 <nguyentrong121004@gmail.com>
2026-06-17 09:26:38 +0700

    Update searchProductByID().c

--- searchProductByID().c
+++ searchProductByID().c
@@ -1,25 +1,26 @@
-#include <stdio.h>
 #include "search.h"
 
-void searchProductByID(Product arr[], int n, int searchID)
-{
+void searchProductByID(int searchID) {
     int found = 0;
+    
+    printf("\nKET QUA TIM KIEM ID %d\n", searchID);
+    printf("--------------------------------------------------------------\n");
 
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i].id == searchID)
-        {
-            printf("\nID : %d", arr[i].id);
-            printf("\nTen: %s", arr[i].name);
-            printf("\nGia: %.2f\n", arr[i].price);
-
+    for(int i = 0; i < productCount; i++) {
+        if(productList[i].productid == searchID) {
+            printf("ID        : %d\n", productList[i].productid);
+            printf("Ten       : %s\n", productList[i].productname);
+            printf("Hang      : %s\n", productList[i].productcomp);
+            printf("Gia       : %d VND\n", productList[i].price);
+            printf("So luong  : %d\n", productList[i].Qnt);
+            
             found = 1;
-            break;
+            break; // Tim thay thi dung vong lap luon
         }
     }
 
-    if(!found)
-    {
-        printf("\nKhong tim thay san pham!\n");
+    if(!found) {
+        printf("Khong tim thay san pham co ID %d!\n", searchID);
     }
+    printf("--------------------------------------------------------------\n");
 }
