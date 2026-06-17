8b2e0fc3a8ada0779f5970a2b6ec7c776c311b46
htrong02 <nguyentrong121004@gmail.com>
2026-06-17 09:27:31 +0700

    Update sortProductsByPrice().c
    Refactor sortProductsByPrice function to accept ascending parameter for sorting order and update bubble sort logic accordingly.

--- sortProductsByPrice().c
+++ sortProductsByPrice().c
@@ -1,22 +1,27 @@
-#include <stdio.h>
 #include "search.h"
 
-void sortProductsByPrice(Product arr[], int n)
-{
-    Product temp;
+void sortProductsByPrice(int ascending) {
+    struct item temp; // Bien tam cung kieu struct item
 
-    for(int i=0;i<n-1;i++)
-    {
-        for(int j=0;j<n-i-1;j++)
-        {
-            if(arr[j].price > arr[j+1].price)
-            {
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+    // Thuat toan Bubble Sort
+    for(int i = 0; i < productCount - 1; i++) {
+        for(int j = 0; j < productCount - i - 1; j++) {
+            
+            // Xac dinh dieu kien hoan vi dua tren bien ascending
+            int condition = ascending ? (productList[j].price > productList[j + 1].price) 
+                                      : (productList[j].price < productList[j + 1].price);
+            
+            if(condition) {
+                temp = productList[j];
+                productList[j] = productList[j + 1];
+                productList[j + 1] = temp;
             }
         }
     }
 
-    printf("\nDa sap xep thanh cong!\n");
+    if(ascending) {
+        printf("\nDa sap xep danh sach san pham theo gia TANG DAN!\n");
+    } else {
+        printf("\nDa sap xep danh sach san pham theo gia GIAM DAN!\n");
+    }
 }
