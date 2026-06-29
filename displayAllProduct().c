54a4ae47a7c86064b67b71360608c5b6c75e10da
htrong02 <nguyentrong121004@gmail.com>
2026-06-29 20:59:27 +0700

    update code

--- displayAllProduct().c
+++ displayAllProduct().c
@@ -16,11 +16,11 @@ void displayAllProduct(void)
     for(int i = 0; i < productCount; i++)
     {
         printf("%-5d %-30s %-15s %-15d %-10d\n", 
-               productList[i].productid, 
-               productList[i].productname, 
-               productList[i].productcomp,
-               productList[i].price, 
-               productList[i].Qnt);
+               productList[i].product_id, 
+               productList[i].product_name, 
+               productList[i].product_brand,
+               productList[i].product_price, 
+               productList[i].product_quantity);
     }
     printf("-----------------------------------------------------------------------------------\n");
 }
